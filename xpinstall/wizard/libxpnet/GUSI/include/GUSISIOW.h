dc1f66e3bebc56ef8ccb9a336f0b2e2e4f6be23d
Shreyas <ss4shreyas@gmail.com>
2025-07-02 11:18:33 +0200

    Initial commit: Mozilla legacy source code

---
+++ xpinstall/wizard/libxpnet/GUSI/include/GUSISIOW.h
@@ -0,0 +1,30 @@
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+// % Project	:	GUSI				-	Grand Unified Socket Interface                    
+// % File		:	GUSISIOW.nw			-	SIOW Interface                                
+// % Author	:	Matthias Neeracher                                           
+// % Language	:	C++                                                        
+// %                                                                       
+// % $Log: GUSISIOW.h,v $
+// % Revision 1.1.114.1  2002/04/10 03:45:40  cltbld%netscape.com
+// % force commit to allow checkouts by branch and date
+// %
+// % Revision 1.1  2001/03/11 22:38:11  sgehani%netscape.com
+// % First Checked In.
+// %                                                 
+// % Revision 1.2  1999/08/26 05:45:08  neeri                              
+// % Fixes for literate edition of source code                             
+// %                                                                       
+// % Revision 1.1  1999/07/19 06:17:08  neeri                              
+// % Add SIOW support                                                      
+// %                                                                       
+//                                                                         
+// \chapter{SIOW Support}                                                  
+//                                                                         
+// SIOW support is based on MPW support, adding a few event hooks so update and activate events
+// get handled during blocking calls. SIOW support is installed implecitly through [[GUSIDefaultSetupConsole]].
+//                                                                         
+// <GUSISIOW.h>=                                                           
+#ifndef _GUSISIOW_
+#define _GUSISIOW_
+
+#endif /* _GUSISIOW_ */
